0e786b3d186852f8ce745b2ea5eda86f9b8a312b
radion358 <radoslaw.dauksza@gmail.com>
2019-01-19 15:49:31 +0100

    Task 18.4 @RequestMapping for TaskController using @RequestParam

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -15,23 +15,23 @@ public class TaskController {
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTask/{id}")
-    public TaskDto getTask(@PathVariable("id") long taskId) {
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
+    public TaskDto getTask(@RequestParam(value = "id") long taskId) {
         return new TaskDto(taskId, "Test title", "test_content");
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask/{id}")
-    public void deleteTask(@PathVariable("id") long taskId) {
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
+    public void deleteTask(@RequestParam(value = "id") long taskId) {
 
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateTask/{taskDto}")
-    public TaskDto updateTask(@PathVariable("taskDto") TaskDto taskDto) {
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    public TaskDto updateTask(@RequestParam(value = "taskDto") TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "test content");
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createTask/{taskDto}")
-    public void createTask(@PathVariable("taskDto") TaskDto taskDto) {
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    public void createTask(@RequestParam(value = "taskDto") TaskDto taskDto) {
 
     }
 }
