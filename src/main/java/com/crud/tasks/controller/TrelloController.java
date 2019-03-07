a42e0d3bc5c4ff086c54250ec9acd9b74768f9ca
radion358 <radoslaw.dauksza@gmail.com>
2019-03-07 20:48:33 +0100

    Task 26.2 rest template

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,38 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.exception.TrelloBoardNotFoundException;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/trello")
+public class TrelloController {
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards().orElseThrow(TrelloBoardNotFoundException::new);
+
+        for (TrelloBoardDto trelloBoardDto : trelloBoards) {
+            if (isKodillaBoard(trelloBoardDto)) {
+                System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+            }
+        }
+
+    }
+
+    private boolean isKodillaBoard(TrelloBoardDto trelloBoardDto) {
+        if (trelloBoardDto.getId() == null) return false;
+        if (trelloBoardDto.getName() == null) return false;
+        return trelloBoardDto.getName().contains("Kodilla");
+    }
+}
---
+++ src/main/java/com/crud/tasks/exception/TrelloBoardNotFoundException.java
@@ -0,0 +1,4 @@
+package com.crud.tasks.exception;
+
+public class TrelloBoardNotFoundException extends RuntimeException {
+}
